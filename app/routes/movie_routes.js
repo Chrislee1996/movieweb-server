6d403e41dbfc1555371610bff52b80642a2cd414
chrislee1996 <prepchrislee@gmail.com>
2022-05-11 21:49:55 -0400

    route for movies work

--- app/routes/movie_routes.js
+++ app/routes/movie_routes.js
@@ -9,15 +9,16 @@ const customErrors = require('../../lib/custom_errors')
 
 // we'll use this function to send 404 when non-existant document is requested
 const handle404 = customErrors.handle404
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
+const axios = require('axios')
 const router = express.Router()
 
 //Fetches all the movies currently playing
 router.get('/:apiKey', (req,res,next) => {
-    const apiKey = req.params.APIKEY
-    console.log(req.params.APIKEY,'apikey')
-    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
-    fetch(url)
+    const apiKey = req.params.apiKey
+    console.log(req.params.apiKey,'apikey')
+    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`)
+    // fetch(url)
         .then((response)=>{
             res.status(201).json(response.data)
         })
